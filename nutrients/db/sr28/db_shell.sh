6921199ad05f2407b50af592c9122fbe01c6127c
Blake La Pierre <blakelapierre@gmail.com>
2017-02-02 10:53:11 -0800

    Script for accessing db shell inside postgresql docker container

---
+++ nutrients/db/sr28/db_shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+POSTGRES_HOST=postgres-usda
+POSTGRES_PORT=5432
+POSTGRES_USER=postgres
+POSTGRES_DATABASE=usda
+
+docker run --rm -it \
+          --link postgres-usda \
+         postgres:9.5 /bin/bash -c "psql -h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER -d $POSTGRES_DATABASE"
